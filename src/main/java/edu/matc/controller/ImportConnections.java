aa49288798f38d4340321d20be4105402fcb16b1
nicolesteck <steck.n@gmail.com>
2018-03-14 19:33:36 -0500

    recreating the details

--- src/main/java/edu/matc/controller/ImportConnections.java
+++ src/main/java/edu/matc/controller/ImportConnections.java
@@ -1,4 +1,52 @@
 package edu.matc.controller;
 
-public class ImportConnections {
+import edu.matc.util.PropertiesLoaderInterface;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
+
+@WebServlet(
+        name = "importConnections",
+        urlPatterns = {"/importConnections"}
+)
+
+public class ImportConnections extends HttpServlet implements PropertiesLoaderInterface {
+
+    Properties properties;
+
+    public void init() throws ServletException {
+        properties = loadProperties("/resources/clientKey.properties");
+    }
+
+    /**
+     *  doGet takes data from the properties object and outputs it to the page
+     *
+     *@param  request               the HttpServletRequest object
+     *@param  response              the HttpServletResponse response
+     *@exception  ServletException  if there is a Servlet failure
+     *@exception  IOException       if there is an I/O failure
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.setAttribute("props3", properties);
+        response.setContentType("text/html");
+        // set the response type before sending data
+
+
+
+        String url = "/importConnections.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
+
+
+        //log("The logged count is " + counter);
+    }
+
 }
--- src/main/java/edu/matc/util/PropertiesLoaderInterface.java
+++ src/main/java/edu/matc/util/PropertiesLoaderInterface.java
@@ -1,4 +1,26 @@
 package edu.matc.util;
 
-public class PropertiesLoaderInterface {
+import java.io.IOException;
+import java.util.Properties;
+
+public interface PropertiesLoaderInterface {
+
+    default Properties loadProperties(String propertiesFilePath)  {
+
+        Properties properties = new Properties();
+
+        try {
+            //System.out.println(propertiesFilePath + " is the filepath");
+            properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
+        } catch(IOException ioException) {
+            System.out.println("Can't load the properties file" + ioException);
+            ioException.printStackTrace();
+            return null;
+        } catch(Exception exception) {
+            System.out.println("Problem: " + exception);
+            exception.printStackTrace();
+            return null;
+        }
+        return properties;
+    }
 }
